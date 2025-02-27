9880cfb567bcd89cd217fd7ade5825037c865a62
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-02-27 09:16:32 +0700

    Use uwquantreg With (In) R Software

---
+++ uwquantreg/uwquantreg.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Fit unit Weibull quantile regression model Use uwquantreg With (In) R Software
+install.packages("remotes")
+remotes::install_github("AndrMenezes/uwquantreg")
+library("uwquantreg")
+spml_one = read.csv("https://raw.githubusercontent.com/timbulwidodostp/uwquantreg/main/uwquantreg/uwquantreg.csv",sep = ";")
+# Estimation Fit unit Weibull quantile regression model Use uwquantreg With (In) R Software
+uwquantreg <- uwquantreg(phpws ~ mhdi + incpc + region + log(pop), data = uwquantreg, tau = 0.5, link = "logit")
+summary(uwquantreg)
+# Fit unit Weibull quantile regression model Use uwquantreg With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
